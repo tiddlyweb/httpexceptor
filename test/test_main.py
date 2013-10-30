3294eb294f8f585a12f429efd98c27fc8359c705
Chris Dent <chris.dent@gmail.com>
2013-10-30 16:31:09 +0000

    StringIO from io in python3

--- test/test_main.py
+++ test/test_main.py
@@ -5,7 +5,10 @@ except ImportError:
 
 import httpexceptor
 
-from StringIO import StringIO
+try:
+    from StringIO import StringIO
+except ImportError:
+    from io import StringIO
 
 
 def mock_response(error=None, message=None):
